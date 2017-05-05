06c261b9751f5e12cf0259cf4011ba21b132c9a5
DeepAQ <DeepAQ@users.noreply.github.com>
2017-05-05 14:15:07 +0800

    change unit to 淮海路

--- mosaik_calc_distance.user.js
+++ mosaik_calc_distance.user.js
@@ -5,24 +5,28 @@
 // @description  try to take over the world!
 // @author       DeepAQ
 // @match        *://ingressmosaik.com/search?*
+// @match        *://ingressmosaik.com/CN/*
 // @grant        none
 // ==/UserScript==
 
 (function() {
     'use strict';
+    
+    var huaihailu = 2300;
 
     $('.well').each(function () {
         var a = $(this).html().match(/(\d+) \(.*, (.+)m\)/i);
+        if (!a || a.length < 3) return;
         if (a[2].endsWith('k')) {
             a[2] = a[2].substr(0, a[2].length - 1) * 1000;
         }
-        var b = Math.round(a[2] / a[1]);
-        if (b < 300) {
-            $(this).append('<span style="color:#00FF00;">' + b + '</span>');
-        } else if (b < 400) {
-            $(this).append('<span style="color:#FF0000;">' + b + '</span>');
+        var b = Math.round(a[2] / a[1]) / huaihailu;
+        if (b < 300 / huaihailu) {
+            $(this).append('<span style="color:#00FF00;">' + b * 100 + '</span>');
+        } else if (b < 400 / huaihailu) {
+            $(this).append('<span style="color:#FF0000;">' + b * 100 + '</span>');
         } else {
-            $(this).append(b);
+            $(this).append(b * 100);
         }
     });
 })();
