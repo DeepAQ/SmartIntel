a42bf60497dfcd0becd46e7e5fe2b5fbb7d17f2f
DeepAQ <DeepAQ@users.noreply.github.com>
2017-10-03 21:48:17 +0800

    Create opr_show_exif_info.user.js

---
+++ opr_show_exif_info.user.js
@@ -0,0 +1,42 @@
+// ==UserScript==
+// @name         OPR Show EXIF Info
+// @version      0.1
+// @namespace    https://github.com/DeepAQ/SmartIntel
+// @description  Show EXIF info of photos in OPR
+// @match        *://opr.ingress.com/recon
+// @grant        GM_XML
+// @require      https://cdn.bootcss.com/zepto/1.2.0/zepto.min.js
+// @require      https://cdn.bootcss.com/exif-js/2.3.0/exif.min.js
+// ==/UserScript==
+
+(function() {
+    var tagsToShow = ['DateTime', 'Make', 'Model', 'Software', 'GPSLatitude', 'GPSLongitude'];
+
+    var getExif = function (url) {
+        var img = document.createElement('img');
+        img.src = url;
+        img.onload = function () {
+            EXIF.getData(img, function () {
+                var tags = EXIF.getAllTags(this);
+                var info = '<strong>[EXIF]</strong><br>';
+                for (var key in tagsToShow) {
+                    if (tags[tagsToShow[key]]) {
+                        info += '<strong>' + tagsToShow[key] + ': </strong>' + tags[tagsToShow[key]] + '<br>';
+                    }
+                }
+                $('#descriptionDiv').append('<div>' + info + '</div>');
+            });
+        };
+    };
+
+	var tryGetImgUrl = function () {
+        var url = $('.center-cropped-img').attr('src');
+        if (!url) {
+            setTimeout(tryGetImgUrl, 1000);
+        } else {
+            getExif((url + '=s0').replace('http:', 'https:'));
+        }
+    };
+
+    tryGetImgUrl();
+})();
