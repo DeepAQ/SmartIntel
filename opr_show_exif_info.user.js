c20baf06d73912d8e649e640f9cd6d1e8cdc9240
DeepAQ <DeepAQ@users.noreply.github.com>
2018-04-08 12:10:31 +0800

    Update opr_show_exif_info.user.js

--- opr_show_exif_info.user.js
+++ opr_show_exif_info.user.js
@@ -8,18 +8,15 @@
 // @require      https://cdn.bootcss.com/exif-js/2.3.0/exif.min.js
 // ==/UserScript==
 
-function dms_to_deg(dms)
-{
-    var d = dms[0];
-    var m = dms[1];
-    var s = dms[2];
-
-    var deg = d/1 + m/60 + s/3600;
-    return deg;
-}
-
 (function() {
     var tagsToShow = ['DateTime', 'Make', 'Model', 'Software', 'GPSLatitude', 'GPSLongitude'];
+
+    var dms_to_deg = function (dms) {
+        var d = dms[0];
+        var m = dms[1];
+        var s = dms[2];
+        return Math.round((d / 1 + m / 60 + s / 3600) * 1E6) / 1E6;
+    };
 
     var getExif = function (url) {
         var img = document.createElement('img');
@@ -30,7 +27,11 @@ function dms_to_deg(dms)
                 var info = '';
                 for (var key in tagsToShow) {
                     if (tags[tagsToShow[key]]) {
-                        info += tagsToShow[key] + ': ' + tags[tagsToShow[key]] + '<br />';
+                        info += tagsToShow[key] + ': ' + tags[tagsToShow[key]];
+                        if (tagsToShow[key] == 'GPSLatitude' || tagsToShow[key] == 'GPSLongitude') {
+                            info += ' (' + dms_to_deg(tags[tagsToShow[key]]) + ')';
+                        }
+                        info += '<br />';
                     }
                 }
                 if (info !== '') {
@@ -38,8 +39,7 @@ function dms_to_deg(dms)
                 } else {
                     info = '<small class="gold">[EXIF]</small><br />No EXIF data present';
                 }
-                if (tags['GPSLatitude'] && tags['GPSLongitude'])
-                {
+                if (tags['GPSLatitude'] && tags['GPSLongitude']) {
                     var deglat = dms_to_deg(tags['GPSLatitude']);
                     var deglon = dms_to_deg(tags['GPSLongitude']);
                     info += '<div class="btn-group"><a class="button btn btn-default" target="osm" href="https://www.openstreetmap.org/?mlat=' + deglat + '&amp;mlon=' + deglon + '&amp;zoom=15">Open in OSM</a></div>';
