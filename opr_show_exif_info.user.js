cc788a8099b84a7e41cc6f55324160ccc18433a9
DeepAQ <i@aq741.com>
2019-02-12 00:11:22 +0800

    Replace exif-js with ExifReader, add console logs

--- opr_show_exif_info.user.js
+++ opr_show_exif_info.user.js
@@ -1,22 +1,15 @@
 // ==UserScript==
 // @name         OPR Show EXIF Info
-// @version      0.4.2
+// @version      0.5
 // @namespace    https://github.com/DeepAQ/SmartIntel
 // @description  Show EXIF info of photos in OPR
 // @match        *://opr.ingress.com/recon
-// @require      https://cdn.bootcss.com/zepto/1.2.0/zepto.min.js
-// @require      https://cdn.bootcss.com/exif-js/2.3.0/exif.min.js
+// @require      https://cdnjs.cloudflare.com/ajax/libs/zepto/1.2.0/zepto.min.js
+// @require      https://github.com/mattiasw/ExifReader/releases/download/2.7.0/exif-reader.js
 // ==/UserScript==
 
 (function() {
-    var tagsToShow = ['DateTime', 'Make', 'Model', 'Software', 'GPSLatitude', 'GPSLongitude'];
-
-    var dms_to_deg = function (dms) {
-        var d = dms[0];
-        var m = dms[1];
-        var s = dms[2];
-        return Math.round((d / 1 + m / 60 + s / 3600) * 1E6) / 1E6;
-    };
+    var tagsToShow = ['DateTime', 'Make', 'Model', 'Software'];
 
     var distance = function (lat1, lng1, lat2, lng2) {
         var dx = Math.abs(lng1 - lng2);
@@ -24,53 +17,58 @@
         var b = (lat1 + lat2) / 2.0;
         var lx = (dx / 180.0 * Math.PI) * 6367000.0 * Math.cos(b / 180.0 * Math.PI);
         var ly = 6367000.0 * dy / 180.0 * Math.PI;
-        // var l = Math.sqrt(lx * lx + ly * ly);
         return (lat1 < lat2 ? '⇑' : '⇓') + Math.ceil(ly) + 'm ' + (lng1 < lng2 ? '⇒' : '⇐') + Math.ceil(lx) + 'm';
     };
 
     var getExif = function (url) {
-        var img = document.createElement('img');
-        img.src = url;
-        img.onload = function () {
-            EXIF.getData(img, function () {
-                var tags = EXIF.getAllTags(this);
+        console.log('[EXIF] Downloading full size photo...');
+        fetch(url).then(function (req) {
+            return req.arrayBuffer();
+        }).then(function (buf) {
+            var info = '';
+            try {
+                var tags = ExifReader.load(buf);
+                console.log('[EXIF] tags:');
                 console.log(tags);
-                var info = '';
                 for (var key in tagsToShow) {
                     if (tags[tagsToShow[key]]) {
-                        info += tagsToShow[key] + ': ' + tags[tagsToShow[key]];
+                        info += tagsToShow[key] + ': ' + tags[tagsToShow[key]].description;
                         if (tagsToShow[key] == 'GPSLatitude' || tagsToShow[key] == 'GPSLongitude') {
-                            var ref = tags[tagsToShow[key] + 'Ref'];
+                            var ref = tags[tagsToShow[key] + 'Ref'].value[0];
                             var negative = (ref == 'S' || ref == 'W');
-                            info += ref + ' (' + (negative ? -1 : 1) * dms_to_deg(tags[tagsToShow[key]]) + ')';
+                            info += ref + ' (' + (negative ? -1 : 1) * tags[tagsToShow[key]].description + ')';
                         }
                         info += '<br />';
                     }
                 }
-                if (info !== '') {
-                    info = '<small class="gold">[EXIF]</small><br>' + info;
-                } else {
-                    info = '<small class="gold">[EXIF]</small><br />No EXIF data present';
-                }
                 if (tags['GPSLatitude'] && tags['GPSLongitude']) {
-                    var deglat = (tags['GPSLatitudeRef'] == 'S' ? -1 : 1) * dms_to_deg(tags['GPSLatitude']);
-                    var deglon = (tags['GPSLongitudeRef'] == 'W' ? -1 : 1) * dms_to_deg(tags['GPSLongitude']);
+                    var deglat = (tags['GPSLatitudeRef'].value[0] == 'S' ? -1 : 1) * tags['GPSLatitude'].description;
+                    var deglon = (tags['GPSLongitudeRef'].value[0] == 'W' ? -1 : 1) * tags['GPSLongitude'].description;
+                    info += 'GPSLatitude: ' + deglat + '<br />GPSLongitude: ' + deglon + '<br />';
                     var portalPos = /@([^,]+),([^,]+)$/.exec(document.evaluate("//*[@ng-bind='subCtrl.pageData.streetAddress']/parent::a").iterateNext().search);
                     var portalLat = Number(portalPos[1]);
                     var portalLng = Number(portalPos[2]);
                     info += 'Distance (portal -> photo): ' + distance(portalLat, portalLng, deglat, deglon) + '<br />';
-                    info += '<div class="btn-group"><a class="button btn btn-default" target="osm" href="https://www.openstreetmap.org/?mlat=' + deglat + '&amp;mlon=' + deglon + '&amp;zoom=15">Open in OSM</a></div>';
+                    // info += '<div class="btn-group"><a class="button btn btn-default" target="osm" href="https://www.openstreetmap.org/?mlat=' + deglat + '&amp;mlon=' + deglon + '&amp;zoom=15">Open in OSM</a></div>';
                 }
-                $('#descriptionDiv').append('<div>' + info + '</div>');
-            });
-        };
+            } catch (e) {}
+
+            if (info !== '') {
+                info = '<small class="gold">[EXIF]</small><br>' + info;
+            } else {
+                info = '<small class="gold">[EXIF]</small><br />No EXIF data present';
+            }
+            $('#descriptionDiv').append('<div>' + info + '</div>');
+        });
     };
 
     var tryGetImgUrl = function () {
         var url = $('.center-cropped-img').attr('src');
         if (!url) {
+            console.log('[EXIF] Waiting for photo to be loaded...');
             setTimeout(tryGetImgUrl, 1000);
         } else {
+            console.log('[EXIF] Photo URL: ' + url);
             getExif((url + '=s0').replace('http:', 'https:'));
         }
     };
