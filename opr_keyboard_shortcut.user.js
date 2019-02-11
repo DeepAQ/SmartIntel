b998f24dad26ca2d65174be0bd727a79bca34ad7
DeepAQ <i@aq741.com>
2019-02-12 00:14:22 +0800

    Update opr_keyboard_shortcut.user.js

--- opr_keyboard_shortcut.user.js
+++ opr_keyboard_shortcut.user.js
@@ -1,15 +1,16 @@
 // ==UserScript==
 // @name         OPR Keyboard Shortcut
-// @version      0.2
+// @version      0.3
 // @namespace    https://github.com/DeepAQ/SmartIntel
 // @description  Add keyboard shortcut support to OPR
 // @match        *://opr.ingress.com/recon
 // @grant        none
-// @require      https://code.jquery.com/jquery-3.2.1.min.js
+// @require      https://cdnjs.cloudflare.com/ajax/libs/zepto/1.2.0/zepto.min.js
 // ==/UserScript==
 
 (function() {
 	var items = ['Total', 'Title', 'Significance', 'Uniqueness', 'Location', 'Accessibility'];
+    var btns = [0, 4, 5, 6, 10, 11];
 	var current = 0;
 	var updateItem = function () {
         $('#current_item').html(items[current]);
@@ -19,11 +20,15 @@
     $('body').on('keydown', function (e) {
 		if (e.keyCode == 13 && e.ctrlKey) {
 			$('.big-submit-button').click();
+            setInterval(function () {
+                if ($('.modal-dialog').length > 0) {
+                    window.location.reload();
+                }
+            }, 500);
 			return;
 		}
 		if (e.key >= '1' && e.key <= '5') {
-            var newPos = current < 4 ? current : current + 3;
-			$('.btn-group').eq(newPos).find('button').eq(e.key - '1').click();
+			$('.btn-group').eq(btns[current]).find('button').eq(e.key - '1').click();
 			current = (current + 1) % items.length;
 			updateItem();
 			return;
